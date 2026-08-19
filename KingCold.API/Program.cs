4ab32201bf83a76c8b78e62db1ca972bd628b26f
Ner Aguilar <ner.aguilar06@gmail.com>
2026-08-18 23:30:03 -0600

    fix: enable CORS for frontend

--- KingCold.API/Program.cs
+++ KingCold.API/Program.cs
@@ -12,14 +12,26 @@ builder.Services.AddDbContext<KingColdDbContext>(options =>
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
